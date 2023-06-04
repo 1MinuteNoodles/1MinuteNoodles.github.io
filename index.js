574e23cd1c8016162d84c535ad78821c4015ade5
IntrntSrfr <aleksander@tosbakken.com>
2023-06-04 06:15:43 +0200

    hell

--- index.js
+++ index.js
@@ -1,46 +1,59 @@
 window.onload = function () {
-
+    let squares = []
     for (j = 0; j < 500; j++) {
+        const color = Math.floor(Math.random() * 0xffffff).toString(16)
+        const left = Math.random()
+        const top = Math.random() * window.innerHeight - 20
+        const rotSpeed = (Math.random() * 2) + 0.25;
 
-        let i = Math.floor(Math.random() * 0xffffff).toString(16)
-
-        let x = document.createElement('div')
-        x.style.height = '10px'
-        x.style.width = '10px'
-        x.style.left = (Math.random() * window.innerWidth - 10) + 'px'
-        x.style.top = (Math.random() * window.innerHeight - 10) + 'px'
-        x.style.position = 'absolute'
-        x.style.backgroundColor = '#' + i
-
-        setInterval(() => {
-            let i2 = Math.floor(Math.random() * 0xffffff).toString(16)
-            x.style.backgroundColor = '#' + i2;
-            px = parseFloat(x.style.top.substring(0, x.style.top.length - 2));
-            if (px > 900) {
-                x.style.top = '0px'
-            } else {
-                x.style.top = parseFloat(x.style.top.substring(0, x.style.top.length - 2)) + Math.random() * 10 + 'px';
-            }
-        }, 10);
-
-        document.body.appendChild(x)
+        let square = document.createElement('div')
+        square.className = 'square'
+        square.style.top = top + 'px'
+        square.style.backgroundColor = '#' + color
+        square.style.animation = `rotation ${rotSpeed}s infinite linear`
+        
+        squares.push({
+            el: square,
+            top: top,
+            left: left
+        })
     }
+
+    let fragment = document.createDocumentFragment()
+    squares.forEach(s => fragment.appendChild(s.el))
+    document.body.appendChild(fragment)
+
+    function distributeSquares() {
+        squares.forEach(s => {
+            s.el.style.left = (s.left * window.innerWidth - 20) + 'px';
+        });
+    }
+    
+    squares.forEach(s => {
+        const moveSquare = () => {
+            const color = Math.floor(Math.random() * 0xffffff).toString(16)
+            s.el.style.backgroundColor = '#' + color
+            s.top = (s.top + Math.random() * 25) % (window.innerHeight - 20)
+            s.el.style.top = s.top + 'px'
+            setTimeout(moveSquare, Math.random() + 25)
+        }
+        moveSquare()
+    })
 
     setInterval(() => {
         let i = Math.floor(Math.random() * 0xffffff).toString(16)
         document.getElementById("gangnam").style.backgroundColor = '#' + i;
-
     }, 100);
+
+    window.addEventListener('resize', distributeSquares);
+    distributeSquares();
 }
 
 function play() {
-
     let a = document.createElement("audio")
     a.src = "gang.mp3"
     document.body.appendChild(a)
     a.play()
-
-    let i = 0
     setInterval(() => {
         let a = document.createElement("audio")
         a.src = "gang.mp3"
